2aa2f7b97d8f03a8715a1313c78e263d61a3b58a
gameyayaya <gameyayaya@gmail.com>
2018-02-22 20:00:35 +0800

    fin.  template

--- pro3/app3/views.py
+++ pro3/app3/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def hello_view(request):
+    return render(request, 'hello_django.html', {
+        'data': "Hello Django ",
+    })
--- pro3/pro3/settings.py
+++ pro3/pro3/settings.py
@@ -69,6 +69,8 @@ TEMPLATES = [
     },
 ]
 
+
+
 WSGI_APPLICATION = 'pro3.wsgi.application'
 
 
